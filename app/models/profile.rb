83aa4ad6cf5fad662a48a5fd9b46d7782116c090
souzabruna <leonarrangel@gmail.com>
2016-09-16 09:22:49 -0300

    retirado merges

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,12 +1,8 @@
 class Profile < ApplicationRecord
   has_many :skills
-<<<<<<< HEAD
   has_many :jobs
 
   accepts_nested_attributes_for :skills, allow_destroy: true
   accepts_nested_attributes_for :jobs, allow_destroy: true
-=======
-  
-  accepts_nested_attributes_for :skills, allow_destroy: true
->>>>>>> 706b46abaa1cb6db0663d27b927481aae9d66e32
+
 end
